c656a66cb48efe2143a85f9e007e0f69bf7e6bfa
Mina2fast <160785184+Mina2fast@users.noreply.github.com>
2024-07-08 00:09:59 +0300

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 /** TODO: Build the navigation bar using JavaScript **/
-
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navBarItem = document.getElementById('navbar__list');
+  
+    sections.forEach(section => {
+      const newListItem = document.createElement('li');
+      newListItem.textContent = section.dataset.nav;
+      newListItem.addEventListener('click', () => {
+        section.scrollIntoView({ behavior: 'smooth' });
+      });
+      navBarItem.appendChild(newListItem);
+    });
+  });
+  
+  
+  
 /** TODO: Add smooth scrolling **/
 
 /** TODO: Add an active state **/
